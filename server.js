69a3cda6bb6f7aab4a7108d749ac1b77930982ed
sshahram <shirin.shahram23@gmail.com>
2021-04-13 14:37:35 -0700

    connect to the MySQL database

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // dependencies
 const express = require('express');
+const mysql = require('mysql2');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -7,12 +8,18 @@ const app = express();
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
-// test the connection
-// app.get('/', (req, res) => {
-//     res.json({
-//         message: 'Hello World'
-//     });
-// });
+// connect to MySQL database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username,
+        user: 'root',
+        // Your MySQL password
+        password: '',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
