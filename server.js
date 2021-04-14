8dcc54df5bff2ca53fe71a72a7b0ac469e2052db
sshahram <shirin.shahram23@gmail.com>
2021-04-14 00:07:43 -0700

    create the DELETE route

--- server.js
+++ server.js
@@ -21,26 +21,61 @@ const db = mysql.createConnection(
     console.log('Connected to the election database.')
 );
 
-// // get all candidates's information
-// db.query(`SELECT * FROM candidates`, (err, rows) => {
-//     console.log(rows);
-// });
+// get all candidates information
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`;
 
-// // get a single candidate
-// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
-//     if(err) {
-//         console.log(err);
-//     }
-//     console.log(row);
-// });
+    db.query(sql, (err, rows) => {
+        if(err) {
+            res.status(500).json({message: err.message});
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    }); 
+});
 
-// // delete a candidate
-// db.query(`DELETE FROM candidates WHERE id =?`, 1, (err, result) => {
-//     if(err) {
-//         console.log(err);
-//     }
-//     console.log(result);
-// });
+
+// Get a single candidate
+app.get('/api/candidate/:id', (req, res) => {
+    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+  
+    db.query(sql, params, (err, row) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: row
+      });
+    });
+  });
+
+
+// delete a candidate
+app.delete('/api/candidate/:id', (req, res) => {
+    const sql = `DELETE FROM candidates WHERE id =?`;
+    const params = [req.params.id];
+    db.query(sql, params, (err, result) => {
+        if(err) {
+            res.statusMessage(400).json({error: res.message});
+        } else if (!result.affectedRows) {
+            res.json ({
+                message: 'Candidate not found'
+            });
+        } else {
+            res.json({
+                message: 'deleted',
+                changes: result.affectedRows,
+                id: req.params.id
+            });
+        }
+    });
+});
 
 // // create a candidate
 // const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
