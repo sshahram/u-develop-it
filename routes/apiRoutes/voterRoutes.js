56c5c943f86302da4baae295c43c0cdc73e5d059
sshahram <shirin.shahram23@gmail.com>
2021-04-15 14:34:09 -0700

    create put route for voters

--- routes/apiRoutes/voterRoutes.js
+++ routes/apiRoutes/voterRoutes.js
@@ -60,4 +60,33 @@ router.post('/voter', ({body}, res) => {
     });
 });
 
+// add put route to update email for a voter
+router.put('/voter/:id', (req, res) => {
+    // Data Validation
+    const errors = inputCheck(req.body, 'email');
+    if(errors) {
+        res.status(400).json({error: errors});
+        return;
+    }
+
+    const sql = `UPDATE voters SET email = ? WHERE id = ?`;
+    const params = [req.body.email, req.params.id];
+
+    db.query(sql, params, (err, result) => {
+        if(err) {
+            res.status(400).json({error: err.message});
+        } else if (!result.affectedRows) {
+            res.json({
+                message: 'Voter not found'
+            });
+        } else {
+            res.json({
+                message: 'success',
+                data: req.body,
+                changes: result.affectedRows
+            });
+        }
+    });
+});
+
 module.exports = router;
