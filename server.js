918413f6c8ff01f4bb4dbb253c72bcc82a447d54
sshahram <shirin.shahram23@gmail.com>
2021-04-15 00:01:40 -0700

    create API routes for parties

--- server.js
+++ server.js
@@ -22,6 +22,8 @@ const db = mysql.createConnection(
     console.log('Connected to the election database.')
 );
 
+// ====================== Routes for Candidates Starts ========================== //
+
 // get all candidates information
 app.get('/api/candidates', (req, res) => {
     const sql = `SELECT candidates.*, parties.name 
@@ -109,6 +111,64 @@ app.post('/api/candidate', ({ body }, res) => {
     });
 });
 
+// ====================== Routes for Candidates Ends ========================== //
+
+// ====================== Routes for Parties Starts =========================== //
+
+// route for all parties
+app.get('/api/parties', (req, res) => {
+    const sql = `SELECT * FROM parties`;
+    db.query(sql, (err, rows) => {
+        if(err) {
+            res.status(500).json({error: err.message});
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
+// route for single party
+app.get('/api/party/:id', (req, res) => {
+    const sql = `SELECT * FROM parties WHERE id =?`;
+    const params = [req.params.id];
+    db.query(sql, params, (err, row) => {
+        if(err) {
+            res.status(400).json({error: err.message});
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+});
+
+// route for delete a party
+app.delete('/api/party/:id', (req, res) => {
+    const sql = `DELETE FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+    db.query(sql, params, (err, result) => {
+        if(err) {
+            res.status(400).json({error: res.message});
+            // checks if anything was deleted
+        } else if (!result.affectedRows) {
+            res.json({
+                message: 'Party not found'
+            });
+        } else {
+            res.json({
+                message: 'deleted',
+                changes: result.affectedRows,
+                id: req.params.id
+            });
+        }
+    });
+});
+
+// ====================== Routes for Parties Ends =========================== //
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
