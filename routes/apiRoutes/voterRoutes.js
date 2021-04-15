10769b04af3286c946840743380e3c6dd02d76c6
sshahram <shirin.shahram23@gmail.com>
2021-04-15 14:11:35 -0700

    create post route for voters

--- routes/apiRoutes/voterRoutes.js
+++ routes/apiRoutes/voterRoutes.js
@@ -36,4 +36,28 @@ router.get('/voter/:id', (req, res) => {
     });
 });
 
+// add post route
+// users will give us first_name, last_name and email
+router.post('/voter', ({body}, res) => {
+    // Data Validation
+    const errors = inputCheck(body, 'first_name', 'last_name', 'email');
+    if (errors) {
+        res.status(400).json({error: errors});
+        return;
+    }
+    const sql = `INSERT INTO voters (first_name, last_name, email) VALUES (?, ?, ?)`;
+    const params = [body.first_name, body.last_name, body.email];
+
+    db.query(sql, params, (err, result) => {
+        if(err) {
+            res.status(400).json({error: err.message});
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body
+        });
+    });
+});
+
 module.exports = router;
