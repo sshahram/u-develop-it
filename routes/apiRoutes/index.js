bd816d532286d3447ecce8ee1e7005c4acc8e8dd
sshahram <shirin.shahram23@gmail.com>
2021-04-15 12:15:58 -0700

    add party routes to the Route file

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -2,5 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 router.use(require('./candidateRoutes'));
+router.use(require('./partyRoutes'));
 
 module.exports = router;
--- server.js
+++ server.js
@@ -12,63 +12,6 @@ app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use('/api', apiRoutes);
 
-// ====================== Routes for Parties Starts =========================== //
-
-// route for all parties
-app.get('/api/parties', (req, res) => {
-    const sql = `SELECT * FROM parties`;
-    db.query(sql, (err, rows) => {
-        if(err) {
-            res.status(500).json({error: err.message});
-            return;
-        }
-        res.json({
-            message: 'success',
-            data: rows
-        });
-    });
-});
-
-// route for single party
-app.get('/api/party/:id', (req, res) => {
-    const sql = `SELECT * FROM parties WHERE id =?`;
-    const params = [req.params.id];
-    db.query(sql, params, (err, row) => {
-        if(err) {
-            res.status(400).json({error: err.message});
-            return;
-        }
-        res.json({
-            message: 'success',
-            data: row
-        });
-    });
-});
-
-// route for delete a party
-app.delete('/api/party/:id', (req, res) => {
-    const sql = `DELETE FROM parties WHERE id = ?`;
-    const params = [req.params.id];
-    db.query(sql, params, (err, result) => {
-        if(err) {
-            res.status(400).json({error: res.message});
-            // checks if anything was deleted
-        } else if (!result.affectedRows) {
-            res.json({
-                message: 'Party not found'
-            });
-        } else {
-            res.json({
-                message: 'deleted',
-                changes: result.affectedRows,
-                id: req.params.id
-            });
-        }
-    });
-});
-
-// ====================== Routes for Parties Ends =========================== //
-
 // Default response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
