5da3fe95b1bca65264071e395b81aa1c661f373a
sshahram <shirin.shahram23@gmail.com>
2021-04-15 12:10:19 -0700

    add candidate routes to the Route file

--- server.js
+++ server.js
@@ -4,129 +4,13 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 const inputCheck = require('./utils/inputCheck');
 const db = require('./db/connection');
+// import routes
+const apiRoutes = require('./routes/apiRoutes');
 
 // Express middleware
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
-
-// ====================== Routes for Candidates Starts ========================== //
-
-// get all candidates information
-app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT candidates.*, parties.name 
-        AS party_name
-        FROM candidates
-        LEFT JOIN parties
-        ON candidates.party_id = parties.id`;
-
-    db.query(sql, (err, rows) => {
-        if(err) {
-            res.status(500).json({message: err.message});
-            return;
-        }
-        res.json({
-            message: 'success',
-            data: rows
-        });
-    }); 
-});
-
-
-// Get a single candidate
-app.get('/api/candidate/:id', (req, res) => {
-    const sql = `SELECT candidates.*, parties.name 
-        AS party_name
-        FROM candidates
-        LEFT JOIN parties
-        ON candidates.party_id = parties.id
-        WHERE candidates.id = ?`;
-    const params = [req.params.id];
-  
-    db.query(sql, params, (err, row) => {
-      if (err) {
-        res.status(400).json({ error: err.message });
-        return;
-      }
-      res.json({
-        message: 'success',
-        data: row
-      });
-    });
-  });
-
-
-// delete a candidate
-app.delete('/api/candidate/:id', (req, res) => {
-    const sql = `DELETE FROM candidates WHERE id =?`;
-    const params = [req.params.id];
-    db.query(sql, params, (err, result) => {
-        if(err) {
-            res.statusMessage(400).json({error: res.message});
-        } else if (!result.affectedRows) {
-            res.json ({
-                message: 'Candidate not found'
-            });
-        } else {
-            res.json({
-                message: 'deleted',
-                changes: result.affectedRows,
-                id: req.params.id
-            });
-        }
-    });
-});
-
-// create a candidate
-app.post('/api/candidate', ({ body }, res) => {
-    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
-    if (errors) {
-        res.status(400).json({ error: errors });
-        return;
-    }
-    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
-                VALUES(?,?,?)`;
-    const params = [body.first_name, body.last_name, body.industry_connected];
-    db.query(sql, params, (err, result) => {
-        if (err) {
-            res.status(400).json({error: err.message});
-            return;
-        }
-        res.json({
-            message: 'success',
-            data: body
-        });
-    });
-});
-
-// update a candidate's party
-app.put('/api/candidate/:id', (req, res) => {
-    // before we attempt to update database, we should check if party_id is provided
-    const errors = inputCheck(req.body, 'party_id');
-    if(errors) {
-        res.status(400).json({error: errors});
-        return;
-    }
-    const sql = `UPDATE candidates SET party_id = ? WHERE id = ?`;
-    const params = [req.body.party_id, req.params.id];
-    db.query(sql, params, (err, result) => {
-        if(err) {
-            res.status(400).json({error: err.message});
-            // check if a record was found
-        } else if (!result.affectedRows) {
-            res.json({
-                message: 'Candidate not found'
-            });
-        } else {
-            res.json({
-                message: 'success',
-                data: req.body,
-                changes: result.affectedRows
-            });
-        }
-    });
-});
-
-// ====================== Routes for Candidates Ends ========================== //
+app.use('/api', apiRoutes);
 
 // ====================== Routes for Parties Starts =========================== //
 
