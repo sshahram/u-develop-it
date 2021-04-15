aa7e90b399d71ebce62fc35f0fedd29c58f09904
sshahram <shirin.shahram23@gmail.com>
2021-04-15 13:00:54 -0700

    add get route for a single voter

--- routes/apiRoutes/voterRoutes.js
+++ routes/apiRoutes/voterRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const db = require('../../db/connection');
 const inputCheck = require('../../utils/inputCheck');
 
+// get all voters
 router.get('/voters', (req, res) => {
     const sql = `SELECT * FROM voters ORDER BY last_name`;
 
@@ -14,6 +15,23 @@ router.get('/voters', (req, res) => {
         res.json({
             messgae: 'success',
             data: rows,
+        });
+    });
+});
+
+// get single voter
+router.get('/voter/:id', (req, res) => {
+    const sql = `SELECT * FROM voters WHERE id = ?`;
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, row) => {
+        if(err) {
+            res.status(400).json({error: err.message});
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: row
         });
     });
 });
