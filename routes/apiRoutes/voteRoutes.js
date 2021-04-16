fdfde11201271f7cc57413a4fea80623aa1d1054
sshahram <shirin.shahram23@gmail.com>
2021-04-16 00:03:59 -0700

    create API route to get vote totals

--- routes/apiRoutes/voteRoutes.js
+++ routes/apiRoutes/voteRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const db = require('../../db/connection');
 const inputCheck = require('../../utils/inputCheck');
 
+// route to add votes
 router.post('/vote', ({body}, res) => {
     //Data validation
     const errors = inputCheck(body, 'voter_id', 'candidate_id');
@@ -23,6 +24,25 @@ router.post('/vote', ({body}, res) => {
             message: 'success',
             data: body,
             changes: result.affectedRows
+        });
+    });
+});
+
+// route to adds up the votes
+router.get('/votes', (req, res) => {
+    const sql = `SELECT candidates.*, parties.name AS party_name , COUNT(candidate_id) AS count 
+            FROM votes
+            LEFT JOIN candidates ON votes.candidate_id = candidates.id
+            LEFT JOIN parties ON candidates.party_id = parties.id
+            GROUP BY candidate_id ORDER BY count DESC;`;
+    db.query(sql, (err, rows) => {
+        if(err) {
+            res.status(500).json({error: err.message});
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
         });
     });
 });
