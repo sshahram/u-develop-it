59768b77d863b8224f9fb2bef2e4b65f63a6b3f4
sshahram <shirin.shahram23@gmail.com>
2021-05-09 20:14:40 -0700

    add environmental variables

--- db/connection.js
+++ db/connection.js
@@ -1,14 +1,16 @@
 const mysql = require('mysql2');
 
+require('dotenv').config();
+
 // connect to MySQL database
 const db = mysql.createConnection(
     {
         host: 'localhost',
         // Your MySQL username,
-        user: '',
+        user: process.env.DB_USER,
         // Your MySQL password
-        password: '',
-        database: 'election'
+        password: process.env.DB_PW,
+        database: process.env.DB_NAME
     });
 
 module.exports = db;
